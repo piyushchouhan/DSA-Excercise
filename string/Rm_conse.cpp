35cf3fafe2502a987a20363b16a0faa9442ca638
Piyush Chauhan <piyush9695@gmail.com>
2023-01-02 21:41:25 +0530

    File added

---
+++ string/Rm_conse.cpp
@@ -0,0 +1,29 @@
+//Remove all consecutive duplicates from the string
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+string removeDuplicates(string S)
+{
+    int n = S.length();
+    string str = "";
+
+    if (n == 0)
+        return str;
+    // str[0] = "";
+    for(int i = 0; i < n-1; i++)
+        if(S[i] != S[i+1])
+            str = str + S[i+1];
+    return str;
+}
+
+int main()
+{
+    string S1 = "geeksforgeeks";
+    cout << removeDuplicates(S1)<< endl;
+   
+    string S2 = "aabaa";
+    cout << removeDuplicates(S2)<< endl;
+    
+    return 0;
+}

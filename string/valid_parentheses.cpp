3e760f2b5720d17e8fd48ea71b4a3296c01f3a83
Piyush Chauhan <you@example.com>
2023-01-02 09:42:29 +0530

    file added but not complete

---
+++ string/valid_parentheses.cpp
@@ -0,0 +1,33 @@
+// C++ program to check for balanced brackets.
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+int areBracketsBalanced(string expr)
+{
+    stack<char> temp;
+    n = exp.length();
+    for(int i = 0; i < n; i++)
+        if(temp.empty(){
+            //IF stack is empty then push brackets into it
+            temp.push(exp[i])
+        })
+        else if 
+
+}
+
+
+
+
+// Driver code
+int main()
+{
+    string expr = "{()}[]";
+ 
+    // Function call
+    if (areBracketsBalanced(expr))
+        cout << "Balanced";
+    else
+        cout << "Not Balanced";
+    return 0;
+}

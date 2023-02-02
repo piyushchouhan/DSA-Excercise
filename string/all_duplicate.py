1c40c5c5b684a6dd2cb811b3de987bb0ee0faa6e
Piyush Chauhan <piyush9695@gmail.com>
2023-02-02 23:02:56 +0530

    code added

--- string/all_duplicate.py
+++ string/all_duplicate.py
@@ -1,3 +1,23 @@
 # Print all the duplicates in the input string
 
+NO_OF_CHARS = 256
 
+def fillCharCounts(string, count):
+    for i in string:
+        count[ord(i)] += 1
+    return count
+
+
+def printDups(string):
+    count = [0] * NO_OF_CHARS
+    count = fillCharCounts(string, count)
+
+    n = 0
+
+    for i in count:
+        if int(i)  > 1:
+            print(f"{chr(n)} count = {str(i)}")
+        n += 1
+
+string = "test string"
+print(printDups(string))
---
+++ string/all_duplicates.cpp
@@ -0,0 +1,18 @@
+//  Print all the duplicates in the input string
+
+#include <iostream>
+using namespace std;
+
+#define NO_OF_CHARS 256
+
+
+
+
+
+
+int main()
+{
+    char str[] = "test string";
+    cout << printDups(str)<< endl;
+    return 0;
+}

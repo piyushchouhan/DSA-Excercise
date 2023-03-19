1ae4182cf2dbb0936504030f149ef6edae5c3c93
Piyush Chauhan <piyush9695@gmail.com>
2023-03-19 14:33:21 +0530

    file added

--- Array/rm_element.cpp
+++ Array/rm_element.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    
+    int val = 2;
+    int arr[]={1,2,3,2,4,1,2};
+    int n = *(&arr+1)-arr;
+    int count = 0;
+    for( int i =0; i < n; i++)
+    {
+        if(arr[i] != val)
+        {
+            arr[count++] = arr[i];
+        }
+    }
+    cout << count<< endl;
+
 }
